69231ffbe836fb2866fee3a5eadfcbee0270f259
BlindNomad <Thiago>
2014-09-20 13:47:58 -0300

    Novos métodos
    Novos métodos e os métodos agora estão em Inglês.

--- libVideo0-1.c
+++ libVideo0-1.c
@@ -1,41 +1,164 @@
+
+
 #include "libVideo0-1.h"
 
-void vDesenhaRetangulo(int x, int y, int largura, int altura){
+
+
+
+#define FONT_BLACK "\033[30m"
+#define FONT_RED "\033[31m"
+#define FONT_GREEN "\033[32m"
+#define FONT_ORANGE "\033[33m"
+#define FONT_BLUE "\033[34m"
+#define FONT_MAGENTA "\033[35m"
+#define FONT_CYAN "\033[36"
+#define FONT_WHITE "\033[37m"
+
+#define BACKGROUND_BLACK "\033[40m"
+#define BACKGROUND_RED "\033[41m"
+#define BACKGROUND_GREEN "\033[42m"
+#define BACKGROUND_ORANGE "\033[43m"
+#define BACKGROUND_BLUE "\033[44m"
+#define BACKGROUND_MAGENTA "\033[45m"
+#define BACKGROUND_CYAN "\033[46m"
+#define BACKGROUND_WHITE "\033[47m"
+
+#define CLEAR_SCREEN "\033[2J"
+
+void vRectangle(int x, int y, int width, int height) {
 	int l, c;
-	
-	
-	
-	for (l = 1; l < altura -1; l++){
-		printf("\033[%d;%dH|", y+l, x);
-		printf("\033[%d;%dH|", y+l, x+largura-1);
+
+
+
+	for (l = 1; l < height - 1; l++) {
+		printf("\033[%d;%dH|", y + l, x);
+		printf("\033[%d;%dH|", y + l, x + width - 1);
 	}
-	
-	for (c = 0; c < largura; c++){
+
+	for (c = 0; c < width; c++) {
 		printf("\033[%d;%dH-", y, x + c);
+		printf("\033[%d;%dH-", y + height - 1, x + c);
 	}
-	
-	for (c = 0; c < largura; c++){
-		printf("\033[%d;%dH-", y+altura -1, x + c);
-	}
+
 }
 
-int vLerInteiro(int x, int y){
-	
+int vReadInt(int x, int y) {
+
 	printf("\033[%d;%dH", y, x);
 	int opcao;
-	
+
 	scanf("%d", &opcao);
 	return opcao;
 }
 
-void vPrint(int x, int y, char *mensagem){
-	printf("\033[%d;%dH%s", y, x, mensagem);
+void vPrint(int x, int y, char *message) {
+	printf("\033[%d;%dH%s", y, x, message);
 }
 
-char *vLerString(int x, int y){
+char *vReadString(int x, int y) {
 	printf("\033[%d;%dH", y, x);
 	char text[255];
-	
+
 	scanf("%s", text);
 	return text;
+}
+
+char *vReadWithBox(int x, int y, int width, char *originalColor) {
+	int i;
+
+	printf("%s%s", BACKGROUND_WHITE, FONT_BLACK);
+
+	for (i = 0; i < width; i++) {
+		printf("\033[%d;%dH ", y, x + i);
+	}
+
+	printf("\033[%d;%dH", y, x);
+	char text[width];
+
+	scanf("%s", text);
+
+	printf("%s", originalColor);
+	return text;
+}
+
+void vBackgroundBox(int x, int y, int width, int height) {
+
+	int i, i2;
+
+	
+	for (i2 = 0; i2 < height; i2++) {
+		for (i = 0; i < width; i++) {
+			printf("\033[%d;%dH ", y + i2, x + i);
+		}
+	}
+
+}
+
+void vTextBoxInLine(int x, int y, int width, char *originalColor) {
+	int i;
+
+	printf("%s%s", BACKGROUND_WHITE, FONT_BLACK);
+
+	for (i = 0; i < width; i++) {
+		printf("\033[%d;%dH ", y, x + i);
+	}
+
+	printf("\033[%d;%dH", y, x);
+
+	printf("%s", originalColor);
+}
+
+void vSetColor(char *backgroundColor, char *fontColor) {
+	printf("%s%s", backgroundColor, fontColor);
+}
+
+int vCustomMenu(int line, char *itens[], int nItens, char *backgroundColor, char *fontColor) {
+	int largura = 0;
+	int i;
+
+	for (i = 0; i < nItens; i++) {
+		if (largura < strlen(itens[i])) {
+			largura = strlen(itens[i]);
+		}
+	}
+
+	int xInicial = ((78 / 2) - (largura / 2) - 1);
+
+	vSetColor(BACKGROUND_BLACK, BACKGROUND_BLACK);
+	
+	vBackgroundBox(xInicial + 1, line + 1, largura + 5, nItens + 3); 
+	
+	vSetColor(backgroundColor, fontColor);
+	
+	vBackgroundBox(xInicial, line, largura + 5, nItens + 3);
+
+	vRectangle(xInicial, line, largura + 5, nItens + 2);
+
+
+	for (i = 0; i < nItens; i++) {
+		vPrint(xInicial + 2, i + 6, itens[i]);
+
+	}
+	
+	char corOriginal[17];
+	sprintf(corOriginal, "%s%s", backgroundColor, fontColor);
+	
+	vPrint(xInicial, i + 7, "Digite a opcao: ");
+	int opcao;
+
+	scanf("%d", &opcao);
+	return opcao;
+}
+
+void vCustomTitle(char *title, int width, char *backgroundColor, char *fontColor){
+	
+	int xInicial;
+	
+	xInicial = 40 - (width / 2);
+	
+	vSetColor(backgroundColor, fontColor);
+	vBackgroundBox(xInicial, 2, width, 3);
+	vRectangle(xInicial, 2, width, 3);
+	xInicial = 40 - (strlen(title) / 2);
+	vPrint(xInicial, 3, title);
 }
--- libVideo0-1.h
+++ libVideo0-1.h
@@ -1,49 +1,142 @@
 /* 
  * File:   libVideo0-1.h
- * Author: thiagoj
+ * Author: Thiago José Silveira
  *
  * Created on 16 de Setembro de 2014, 17:56
+ * 
+ * Essa lib funciona se baseando no tamanho máximo de tela de 80 colunas,
+ * desculpe pelo o Inglês, não sou muito habilidoso com a lingua ainda.
+ * 
+ * This lib works based on the maximum size of 80-column screen, sorry for the English, I'm not very skilled with the language yet.
  */
 
 #include <stdio.h>
 #include <strings.h>
 #include <stdlib.h>
 
+
 #ifndef LIBVIDEO0_1_H
 #define	LIBVIDEO0_1_H
 
-/**
- * Desenha um retangulo, caracter - na horizontal e | na vertical.
- * @param x Inicio do retangulo na coluna estipulada
- * @param y Inicio do retangulo na linha estipulada
- * @param largura largura em colunas
- * @param altura altura em linhas
- */
-void vDesenhaRetangulo(int x, int y, int largura, int altura);
+#define FONT_BLACK "\033[30m"
+#define FONT_RED "\033[31m"
+#define FONT_GREEN "\033[32m"
+#define FONT_ORANGE "\033[33m"
+#define FONT_BLUE "\033[34m"
+#define FONT_MAGENTA "\033[35m"
+#define FONT_CYAN "\033[36"
+#define FONT_WHITE "\033[37m"
+
+#define BACKGROUND_BLACK "\033[40m"
+#define BACKGROUND_RED "\033[41m"
+#define BACKGROUND_GREEN "\033[42m"
+#define BACKGROUND_ORANGE "\033[43m"
+#define BACKGROUND_BLUE "\033[44m"
+#define BACKGROUND_MAGENTA "\033[45m"
+#define BACKGROUND_CYAN "\033[46m"
+#define BACKGROUND_WHITE "\033[47m"
+
+#define CLEAR_SCREEN "\033[2J"
 
 /**
- * Pega um int na coluna e linha estipulada
- * @param x Inicio na coluna estipulada
- * @param y Inicio na linha estipulada
- * @return retorna o int pego do teclado
+ * Desenha um retangulo com usando o caracter "-" na horizontal e o "|" na vertical
+ * Draws a rectangle with using the "-" character in the horizontal and the "|" vertical
+ * @param x
+ * @param y
+ * @param width
+ * @param height
  */
-int vLerInteiro(int x, int y);
+void vRectangle(int x, int y, int width, int height);
 
 /**
- * Imprime uma string na linha e coluna estipulada.
- * @param x  Inicio na coluna estipulada
- * @param y Inicio na linha estipulada
- * @param mensagem
+ * Le um inteiro nas coordenadas x e y
+ * Reads entire in x and y coordinates
+ * @param x
+ * @param y
+ * @return 
  */
-void vPrint(int x, int y, char *mensagem);
-/**
- * Le uma string na linha e coluna estipulada.
- * @param x Inicio na coluna estipulada
- * @param y Inicio na linha estipulada
- * @return retorna uma string
- */
-char *vLerString(int x, int y);
+int vReadInt(int x, int y);
 
+/**
+ * Imprime a mensagem na tela na coordenadas x e y
+ * Prints the message on the screen at coordinates x and y
+ * @param x
+ * @param y
+ * @param message
+ */
+void vPrint(int x, int y, char *message);
+
+/**
+ * Lê um cadeia de caracteres nas cordenadas x e y
+ * Reads a string in the x and y coordinates
+ * @param x
+ * @param y
+ * @return 
+ */
+char *vReadString(int x, int y);
+
+/**
+ * Desenha uma caixa na altura de 1 linha com o fundo branco.
+ * Draws a box in the height of 1 line with the white background.
+ * @param x
+ * @param y
+ * @param width
+ * @param originalColor
+ * @return 
+ */
+char *vReadWithBox(int x, int y, int width, char *originalColor);
+
+/**
+ * Desenha um caixa branca com a altura de 1 linha e lê uma cadeia de caracteres do teclado.
+ * Draw a white box with a height of one line and reads string from the keyboard.
+ * @param x
+ * @param y
+ * @param width
+ * @param originalColor
+ */
+void vTextBoxInLine(int x, int y, int width, char *originalColor);
+
+/**
+ * Altera a cor do fundo e a cor da letra
+ * Changes the background color and the font color
+ * @param backgroundColor
+ * @param fontColor
+ */
+void vSetColor(char *backgroundColor, char *fontColor);
+
+
+/**
+ * Cria um menu com sombra e cores
+ * Creates a menu with shadow and colors
+ * @param line
+ * @param itens
+ * @param nItens
+ * @param backgroundColor
+ * @param fontColor
+ * @return 
+ */
+int vCustomMenu(int line, char *itens[], int nItens, char *backgroundColor, char *fontColor);
+
+/**
+ * Desenha uma caixa sem borda, é necessário setar uma cor diferente de fundo antes de usar a função.
+ * Draws a box borderless, it is necessary to set a different background color before using the function.
+ * @param x
+ * @param y
+ * @param width
+ * @param height
+ */
+void vBackgroundBox(int x, int y, int width, int height);
+
+
+/**
+ * Cria um caixa no topo da tela com o titulo no centro.
+ * Creates a box at the top of the screen with the title in the center.
+ * @param title
+ * @param width
+ * @param backgroundColor
+ * @param fontColor
+ */
+void vCustomTitle(char *title, int width, char *backgroundColor, char *fontColor);
 
 #endif	/* LIBVIDEO0_1_H */
 
